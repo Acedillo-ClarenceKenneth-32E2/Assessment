84d9955ed911484506bd5b985218029905d01c0c
kennethacedillo@gmail.com <kennethacedillo@gmail.com>
2024-03-02 12:20:53 +0800

    Initial commit

---
+++ Prelim_32A2_BSIT_ACEDILLOCLARENCEKENNETH/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Prelim_32A2_BSIT_ACEDILLOCLARENCEKENNETH.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
